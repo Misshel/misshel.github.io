dc885f8c60a036915106d02fd6d4ea00673d1177
Misshel <jenmish094@gmail.com>
2017-07-09 23:04:33 -0500

    colocando carrucel

--- assets/js/init.js
+++ assets/js/init.js
@@ -2,7 +2,9 @@
   $(function(){
 
     $('.button-collapse').sideNav();
+    $('.carousel.carousel-slider').carousel({fullWidth: true});
+    // $('.carousel').carousel('next', 3);
     $('.parallax').parallax();
-
+    console.log("hola");
   }); // end of document ready
 })(jQuery); // end of jQuery name space
